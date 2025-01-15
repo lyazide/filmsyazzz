4f4bda7ebae15f0bd5ff3801171e5ebbda04e7ba
lyazide <47938327+lyazide@users.noreply.github.com>
2025-01-15 13:14:19 +0100

    Additional test and style

--- src/composants/Wishlist.jsx
+++ src/composants/Wishlist.jsx
@@ -14,7 +14,9 @@ const Wishlist = () => {
       {wishlist.map(movie => (
         <div key={movie.id} className={styles.jacket}>
           <h3>{movie.title}</h3>
+          <Link to={`/movie/${movie.id}`}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+          </Link>
           <p>Average Rating: {movie.vote_average}</p>
           <Link to={`/movie/${movie.id}`}> <button className={styles.button}>See Details</button> </Link>
           <button className={styles.button} onClick={() => removeFromWishlist(movie.id)}>Remove</button>
